b466e1b2a6b2f7671dd4faed13b7e06724d45166
pravin135 <thotapraveenbabu@gmail.com>
2023-01-24 16:17:04 +0530

    added deployment

---
+++ horizantalpodscaler-redis-replica.tf
@@ -0,0 +1,18 @@
+resource "kubernetes_horizontal_pod_autoscaler" "redis_replica" {
+  metadata {
+    name      = "redis-replica"
+    namespace = "k8s-workshop"
+  }
+
+  spec {
+    max_replicas                      = 5
+    min_replicas                      = 1
+    target_cpu_utilization_percentage = 20
+
+
+    scale_target_ref {
+      kind = "Deployment"
+      name = "redis-replica"
+    }
+  }
+}
