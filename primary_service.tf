22c899a68d5a8117faeb5448702ec23ddc46668c
pravin135 <thotapraveenbabu@gmail.com>
2023-01-24 14:50:41 +0530

    added deployment

--- primary_service.tf
+++ primary_service.tf
@@ -1,6 +1,6 @@
 resource "kubernetes_service" "redis_primary" {
   metadata {
-    name = "redis_primary"
+    name = "redis-primary"
     namespace = "k8s-workshop"
   }
   spec {
@@ -13,7 +13,5 @@ resource "kubernetes_service" "redis_primary" {
       port        = 6379
       target_port = 6379
     }
-
-    type = "LoadBalancer"
   }
 }
