9c34a353a92465739eabb2eb838fdf1e567e2d84
pravin135 <thotapraveenbabu@gmail.com>
2023-01-24 17:57:33 +0530

    added deployment

--- Networkpolicy-redis-replica.tf
+++ Networkpolicy-redis-replica.tf
@@ -1,31 +1,29 @@
-resource "kubernetes_network_policy" "redis-replica" {
-  metadata {
-    name      = "redis"
-    namespace = "k8s-workshop"
-  }
+# resource "kubernetes_network_policy" "redis-replica" {
+#   metadata {
+#     name      = "redis"
+#     namespace = "k8s-workshop"
+#   }
+#   spec {
+#     pod_selector {
+#       match_labels = {
+#         app = "redis"
+#     #   
+#     }
+#     from {
+#         pod_selector {
+#           match_labels = {
+#             app = "redis"
 
-  spec {
-    pod_selector {
-      match_labels = {
-        app = "redis"
-      }
-      policy_types = ["Ingress"]
-    }
-    from {
-        pod_selector {
-          match_labels = {
-            app = "redis"
-
-          }
-        }
-      }
-    ingress {
-      ports {
-        port     = "6379"
-        protocol = "TCP"
-     }
+#           }
+#         }
+#       }
+#     ingress {
+#       ports {
+#         port     = "6379"
+#         protocol = "TCP"
+#      }
       
 
-    }
-  }
-}
+#     }
+#   }
+# }
