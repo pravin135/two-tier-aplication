c298a33fc78effaa081d5fca33184429cd60ada6
pravin135 <thotapraveenbabu@gmail.com>
2023-01-24 15:44:00 +0530

    added deployment

---
+++ replica-service.tf
@@ -0,0 +1,21 @@
+resource "kubernetes_service" "redis_replica" {
+  metadata {
+    name      = "redis-replica"
+    namespace = "k8s-workshop"
+  }
+  labels {
+    app  = "redis"
+      role = "replica"
+      tier = "backend"
+  }
+  spec {
+    selector = {
+      app  = "redis"
+      role = "replica"
+      tier = "backend"
+    }
+    port {
+      port        = 6379
+    }
+  }
+}
