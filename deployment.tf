1cdd9571190672ec160aab896f42b5a9e11d8128
pravin135 <thotapraveenbabu@gmail.com>
2023-01-24 12:45:11 +0530

    added deployment

---
+++ deployment.tf
@@ -0,0 +1,33 @@
+resource "kubernetes_deployment" "redis-primary" {
+  metadata {
+    name = "redis-primary"
+    namespace = "k8s-workshop"
+  }
+  spec {
+    replicas = 1
+    template {
+      metadata {
+        labels = {
+          "app" = "redis"
+          "role" = "primary"
+          "tier" = "backend"
+        }
+      }
+      spec {
+        containers {
+          name  = "redis"
+          image = "gcr.io/google_containers/redis:e2e"
+        }
+      }
+           resources {
+            limits = {
+              cpu    = "100"
+              memory = "100Mi"
+            }
+            }
+          port {
+            container_port = 6379
+          }
+        }
+      }
+}
